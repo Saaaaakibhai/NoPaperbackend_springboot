b27fd272602c3e968e311b83004a930b36a6df4d
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2025-05-18 12:05:06 +0600

    Update

--- nopaperbackend/src/main/java/com/example/nopaperbackend/repository/UserRepository.java
+++ nopaperbackend/src/main/java/com/example/nopaperbackend/repository/UserRepository.java
@@ -8,8 +8,19 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends CrudRepository<User,Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query(value = "SELECT email FROM users WHERE email = :email ", nativeQuery = true)
+    List<String> checkUserEmail(@Param("email") String email);
+
+    @Query(value = "SELECT password FROM users WHERE email = :email", nativeQuery = true)
+    String checkUserPasswordByEmail(@Param("email") String email);
+
+    @Query(value = " SELECT * FROM users WHERE email = :email", nativeQuery = true)
+    User GetUserDetailsByEmail(@Param("email") String email);
 
     @Transactional
     @Modifying
@@ -18,5 +29,6 @@ public interface UserRepository extends CrudRepository<User,Integer> {
                         @Param("last_name") String last_name,
                         @Param("email") String email,
                         @Param("password") String password);
+
 
 }
--- nopaperbackend/src/main/java/com/example/nopaperbackend/services/UserService.java
+++ nopaperbackend/src/main/java/com/example/nopaperbackend/services/UserService.java
@@ -1,13 +1,14 @@
 package com.example.nopaperbackend.services;
-
-
+import com.example.nopaperbackend.models.User;
 import com.example.nopaperbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 public class UserService {
+
     @Autowired
     UserRepository userRepository;
 
@@ -16,4 +17,19 @@ public class UserService {
     }
     // End Of Register New User Service Method.
 
+
+    public List<String> checkUserEmail(String email){
+        return userRepository.checkUserEmail(email);
+    }
+    // End Of Check User Email Services Method.
+
+    public String checkUserPasswordByEmail(String email){
+        return userRepository.checkUserPasswordByEmail(email);
+    }
+    // End Of Check User Password Services Method.
+
+    public User getUserDetailsByEmail(String email){
+        return userRepository.GetUserDetailsByEmail(email);
+    }
+    // End Of Get User Details By Email.
 }
